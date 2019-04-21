613c8f0c25e209fb6d9b41bf6bfdf7043f61c343
krokofant <krokofant@users.noreply.github.com>
2019-04-21 14:33:34 +0200

    :sparkles: Support copying the unicode emoji

--- src/scripts/clipboard.js
+++ src/scripts/clipboard.js
@@ -1,1 +1,8 @@
-var clipboard = new Clipboard('.gitmoji');
+var clipboard = new Clipboard(".gitmoji", {
+  text: function(trigger) {
+    var shouldCopyUnicode = document.location.search.includes("?unicode");
+    return shouldCopyUnicode
+      ? trigger.textContent
+      : trigger.getAttribute("data-clipboard-text");
+  }
+});
